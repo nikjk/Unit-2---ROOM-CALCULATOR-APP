22b9ae55f3b191e5d64d994b4bc595a867a13d0c
nikjk <nik.kuehn@outlook.com>
2023-04-15 16:47:12 -0400

    Initial push for Unit 2 - Room Calculator App

---
+++ Unit 2 - ROOM CALCULATOR/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Please enter the length of the classroom.");
+
+string userinput = Console.ReadLine();
+
+int length = int.Parse(userinput);
+
+Console.WriteLine();
+
+Console.WriteLine("Please enter the width of the classroom.");
+
+userinput = Console.ReadLine();
+
+int width = int.Parse(userinput);
+
+int perimiter = 2 * (length + width);
+
+Console.WriteLine();
+
+
+int area = length * width;
+
+Console.WriteLine("The perimiter of the classroom is " + perimiter + ".");
+Console.WriteLine();
+
+Console.WriteLine("The area of the classroom is " + area + ".");
+Console.WriteLine();
+
+
+if (area <= 250)
+{
+    Console.WriteLine("Room is small");
+}
+
+else if (area > 250 && area < 650)
+{
+    Console.WriteLine("Room is medium");
+}
+
+else if (area >= 650)
+{
+    Console.WriteLine("Room is large");
+}
